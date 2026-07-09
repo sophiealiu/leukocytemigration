f14d4fb7af55123825aa1615de893d77a2a3118d
Sophie Liu <saliu@mdanderson.org>
2026-07-09 13:05:26 -0500

    Update very preliminary ODE.m

--- very preliminary ODE.m
+++ very preliminary ODE.m
@@ -1,110 +1,130 @@
-% -----------------------------------------------------------------------------
+% ------------------------------------------------------------------------
 % Author: Sophie A. Liu
-% Date : 07/08/2026 6:09pm CDT
+% Date : 07/09/2026 1:04pm CDT
 % Purpose: simple compartment modeling
-% -----------------------------------------------------------------------------
+% ------------------------------------------------------------------------
+
+% --- assumptions --------------------------------------------------------
+% conservation of volume in each compartment
+% homeostasis will be reached
+% once labeled, it cannot be unlabeled (however, label only lasts 5 min and we have 10 min increments...
+% ------------------------------------------------------------------------
+
 
 function dydt = splitComps(t,y,p)
-b = y(1);
+    b = y(1);
 
 % -- continues down similarly, many vars ----
-l1 = y(2);       % bone marrow
-l2 = y(3);       % both red and white pulp spleen
+    l1 = y(2);        % bone marrow
+    l2a = y(3);       % both red and white pulp spleen
+    l2b = y(4);
 
-l3a = y(4);      % lymph axillary
-l3b = y(5);      % lymph iliac
-l3c = y(6);      % lymph inguinal
-l3d = y(7);      % lymph mesenteric
+    l3a = y(5);      % lymph axillary
+    l3b = y(6);      % lymph iliac
+    l3c = y(7);      % lymph inguinal
+    l3d = y(8);      % lymph mesenteric
 
-l4 = y(8);       % peyer's patches
+    l4 = y(9);       % peyer's patches
 
-% --
-u1 = y(9);    
-u2 = y(10);
+% ---
+    u1 = y(10);    
+    u2a = y(11);
+    u2b = y(12);
 
-u3a = y(11);
-u3b = y(12);
-u3c = y(13);
-u3d = y(14);
+    u3a = y(13);
+    u3b = y(14);
+    u3c = y(15);
+    u3d = y(16);
 
-u4 = y(15);
-
-
-% ----------------------------
-
-kneg1 = p(1);                % reverse direction ksub-1
-kneg2 = p(2);                % forward direction k1
-
-kneg3a = p(3);                
-kneg3b = p(4);                
-kneg3c = p(5);                
-kneg3d = p(6);
-
-kneg4 = p(7); 
-
-k1 = p(8);                
-k2 = p(9); 
-
-k3a = p(10);
-k3b = p(11);
-k3c = p(12);
-k3d = p(13);
-
-k4 = p(14); 
+    u4 = y(17);
 
 
-dydt = [-(k1+k2+ k3a+k3b+k3c+k3d+ k4)*b + kneg1*(l1+u1) + kneg2*(l2+u2) + ...
-    kneg3a*(l3a+u3a) + kneg3b*(l3b+u3b) + kneg3c*(l3c+u3c) + + kneg3d*(l3d+u3d) + ...
-    kneg4*(l4 + u4); 
+% --- reverse rates ---
+    kneg1 = p(1);             % bm   
+    kneg2a = p(2);            % spleen   
+    kneg2b = p(3);
 
-        kneg1*u1;
-        k2*b - kneg2*l2;
+    kneg3a = p(4);            % LNs     
+    kneg3b = p(5);                
+    kneg3c = p(6);                
+    kneg3d = p(7);
+
+    kneg4 = p(8);             % PP
+
+% --- forward rates ---
+    k1 = p(9);
+
+    k2a = p(10); 
+    k2b = p(11);
+
+    k3a = p(12);
+    k3b = p(13);
+    k3c = p(14);
+    k3d = p(15);
+
+    k4 = p(16); 
+
+
+% --- system -------------------------------------------------------------
+dydt = [ 
+    % --- all exiting the blood -----------------------------
+    -(k1+ k2a+k2b+ k3a+k3b+k3c+k3d+ k4)*b + ...
+        kneg1*(l1+u1) + ...
+        kneg2a*(l2a+u2a) + kneg2b*(l2b+u2b) + ...
+        kneg3a*(l3a+u3a) + kneg3b*(l3b+u3b) + kneg3c*(l3c+u3c) + kneg3d*(l3d+u3d) + ...
+        kneg4*(l4 + u4); 
+   
+    % --- total labeled flux for each tissue compartment ----
+        k1*b - kneg1*l1;
+        k2a*b - kneg2a*l2a;
+        k2b*b - kneg2b*l2b;
 
         k3a*b - kneg3a*l3a;
         k3b*b - kneg3b*l3b;
         k3c*b - kneg3c*l3c;
-        k3d*b - kneg3d*l3d + kneg4*l4;
+        k3d*b - kneg3d*l3d;
 
         k4*b - kneg4*l4;
 
+    % --- unlabeled exiting each tissue compartment --------
         -kneg1*u1;
-        -kneg2*u2;
-
+        -kneg2a*u2a;
+        -kneg2b*u2b;
         -kneg3a*u3a;
         -kneg3b*u3b;
         -kneg3c*u3c;
         -kneg3d*u3d;
-
-        -kneg4*u4];
+        -kneg4*u4;];
 
 end
 
-% initial guess for params. magnitude not direction
-% could fit a least squares estimate but not sure what that entails
-p = [0.004, 0.022, ...
-    0.0006, 0.0014, 0.00049, 0.00063, ...
-    0.0012, ...
-    % now k forward
-    0.0022, 0.021, ...
-    0.0055, 0.0025, 0.0026, 0.014, ...
-    0.0042];      
+% --- initial guess for params based on R. magnitude not direction -------
+% krevs first then kfors
+p = [0.002370606, ... 
+    0.02488078, 0.001255718, ...
+    0.00092377 , 0.001142701 , 0.0006220911, 0.0006661198, ...
+    0.001302991, ...
 
-% ICs. check matrix carefully
-% y0 = [34969, ... 
-%     0, 0, 0, 0, 0, 0, 0, 0, ... 
-%     19086, 277000, ...
-%     301675, 63998, 186545, ...
-%     128766];
+    0.001349783, ...
+    0.03721345, 0.01029477 , ...
+    0.0162731, 0.003164098 , 0.006141488 , 0.01709096, ...
+    0.00712985];      
 
-% fracs easier and less skewed
-y0 = [1, ... 
-    0, 0, 0, 0, 0, 0, 0, ... 
+% fraction ICs are less skewed
+y0 = [1, ...              % blood
+    0, ...                % bm
+    0, 0, ...             % spleen
+    0, 0, 0, 0, ...       % LNs
+    0, ...                % PPs
+    0, ...
     1, 1, ... 
     1, 1,1,1, ...
     1];      
-tspan = [0, 5000];
 
-% solving
+% --- they can take the experiment out to 3 days (72 hrs, 4320 sec.)
+tspan = [0, 4000];    % rates etc. in 1/s unit
+
+% --- solving
 [t, y] = ode45(@(t, y)splitComps(t, y, p), tspan, y0);
 plot(t,y)
 legend('blood', ...
@@ -115,20 +135,23 @@ legend('blood', ...
     'UL LN axillary', 'UL LN iliac', 'UL LN inguinal', 'UL LN mesenteric',...
     'UL PPs')
 
-UL = sum(y(:,9:15),2);
+UL = sum(y(:,10:17),2);
 
 figure
 plot(t,UL,'LineWidth',2)
 ylabel('Total unlabeled')
 xlabel('Time')
 
-N_BM  = y(:,2);
-N_SPL = y(:,3);
-N_AX  = y(:,4);
-N_IL  = y(:,5);
-N_IG  = y(:,6);
-N_ME  = y(:,7);
-N_PP  = y(:,8);
 
-plot(t,[N_BM N_SPL N_AX N_IL N_IG N_ME N_PP])
-legend('BM','Spleen','Ax','Iliac','Inguinal','Mes','PP')
+% --- labeled will approach completion as t -> inf -----------------------
+N_BM  = y(:,2);
+N_SRP = y(:,3);
+N_SWP = y(:,4);
+N_AX  = y(:,5);
+N_IL  = y(:,6);
+N_IG  = y(:,7);
+N_ME  = y(:,8);
+N_PP  = y(:,9);
+
+plot(t,[N_BM N_SRP, N_SWP, N_AX N_IL N_IG N_ME N_PP])
+legend('BM','Red pulp', 'White pulp','Ax','Iliac','Inguinal','Mes','PP')
